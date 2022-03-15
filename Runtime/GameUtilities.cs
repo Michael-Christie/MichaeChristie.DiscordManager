9a8514626a06960afa4fcbbce996d14aa7d9f63f
Michael Christie <michaelchristie26@gmail.com>
2022-03-15 11:56:43 +0000

    Updates package number for test

--- Runtime/GameUtilities.cs
+++ Runtime/GameUtilities.cs
@@ -4,7 +4,7 @@ namespace MC.DiscordManager
 {
     public class GameUtilities : MonoBehaviour
     {
-        public static string version = "1.0.2";
+        public static string version = "1.0.3";
 
         public const string path = "DiscordManager/DiscordSettings";
     }
