b28daae91f56987cb0f768d40e0c3d670b5f50f5
Michael Christie <michaelchristie26@gmail.com>
2022-06-11 14:27:23 +0100

    update package to 1.0.5

--- Runtime/GameUtilities.cs
+++ Runtime/GameUtilities.cs
@@ -4,7 +4,7 @@ namespace MC.DiscordManager
 {
     public class GameUtilities : MonoBehaviour
     {
-        public static string version = "1.0.3";
+        public static string version = "1.0.5";
 
         public const string path = "DiscordManager/DiscordSettings";
     }
